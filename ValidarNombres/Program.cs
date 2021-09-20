e9d7ac7c7a51ee203c1c5afd494f00bbfefa1c75
Santiago Llanes <sllanesmanuel@gmail.com>
2021-09-20 16:35:37 -0300

    asdas

--- ValidarNombres/Program.cs
+++ ValidarNombres/Program.cs
@@ -10,6 +10,24 @@ namespace ValidarNombres
     {
         static void Main(string[] args)
         {
+            string txt = "";
+
+
+            Console.WriteLine("Ingrese el texto:");
+            txt = Console.ReadLine();
+
+            string analizador = txt.ToUpper();
+
+            if (txt != analizador)
+            {
+                Console.WriteLine("El texto ingresado: {0}, no cumple los requisitos", txt);
+
+            }
+            else
+            {
+                Console.WriteLine("El texto ingresado: {0}, es correcto");
+            }
+                Console.ReadKey();
         }
     }
 }
