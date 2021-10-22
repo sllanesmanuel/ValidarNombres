0c73e4b71e9a221b128503c5519bb9f6b1932c02
Santiago Llanes <sllanesmanuel@gmail.com>
2021-10-21 22:32:08 -0300

    aa

--- ValidarNombres/Program.cs
+++ ValidarNombres/Program.cs
@@ -11,138 +11,169 @@ namespace ValidarNombres
     {
         static void Main(string[] args)
         {
-            var nom1 = "";
-            int cantLetras1 = 0;
-            var nom2 = "";
-            int cantLetras2 = 0;
-            var nom3 = "";
-            int cantLetras3 = 0;
-
+            // Pedimos el nombre y lo separamos en 3 con el split.
             Console.WriteLine("Ingrese un nombre: (EJj: 'Edgar Alan Poe')");
             string cadena = Console.ReadLine();
+            ValidarNombre nombreFinal = new ValidarNombre();
+            Console.WriteLine(nombreFinal.ValidarTodo(cadena));
+
+            Console.ReadKey();
+        }
+    }
+
+    class ValidarNombre
+    {
+
+        public string ValidarTodo(string cadena)
+        {
+            var nom1 = "";
+            var nom2 = "";
+            var nom3 = "";
 
             string[] nombre = cadena.Split(' ');
-
+            // Tenemos los 3 nombres separados listos para evaluarlos.
             if (nombre.Length < 2)
             {
-                Console.WriteLine("Debe ingresar minimo 2 o 3 términos.");
+                return "Por favor, ingresar al menos 2 términos.";
             }
             else
             {
-                foreach (var n in nombre[0])
+                if (nombre.Length <= 2)
                 {
-                    if (n != ' ')
+                    nom1 = nombre[0];
+                    nom2 = nombre[1];
+                    nom3 = "";
+
+                }
+                else
+                {
+                    nom1 = nombre[0];
+                    nom2 = nombre[1];
+                    nom3 = nombre[2];
+                }
+            }
+
+            if (nom1.Length == 1)
+            {
+                return "El primer nombre no es valido ya que es muy pequeño o no cuenta como abreviacion al no incluir '.'.";
+            }
+            if (nom2.Length == 1)
+            {
+                return "El segundo nombre no es valido ya que es muy pequeño o no cuenta como abreviacion al no incluir '.'.";
+            }
+            if (nom3.Length == 1)
+            {
+                return "El apellido no es valido ya que es muy pequeño.";
+            }
+
+            if (ValidadorMayusculas(nom1, nom2, nom3) == "ok")
+            {
+                if (ValidadorLargoYPunto(nom1, nom2, nom3) != "ok")
+                {
+                    return "Nombre no valido.";
+                }
+            }
+            else
+            {
+                return "Nombre no valido.";
+            }
+
+
+
+            return "El nombre es valido";
+        }
+
+        public string ValidadorMayusculas(string PrimerNombre, string SegundoNombre, string Apellido)
+        {
+
+            // Aca evalua si el primer nombre empieza con mayuscula o no.
+            string mayusPrimerNombre = PrimerNombre.Substring(0, 1).ToUpper();
+
+            if (PrimerNombre.Substring(0, 1) != mayusPrimerNombre && PrimerNombre.Length != 2)
+            {
+                return "El primer nombre es incorrecto, debe empezar con mayusculas y solo puede contener 1 letra.";
+            }
+
+            // Aca evalua si el segundo nombre empieza con mayuscula o no.
+            string mayusSegundoNombre = SegundoNombre.Substring(0, 1).ToUpper();
+
+            if (SegundoNombre.Substring(0, 1) != mayusSegundoNombre && SegundoNombre.Length != 2)
+            {
+                return "El segundo nombre es incorrecto, debe emplezar con mayusculas.";
+            }
+
+            // Aca evalua si el  apellido empieza con mayuscula o no.
+            if (Apellido != "")
+            {
+                string mayusApellido = Apellido.Substring(0, 1).ToUpper();
+                if (Apellido.Length <= 2)
+                {
+                    return "El apellido no puede ser abreviado.";
+                }
+                else
+                {
+                    if (Apellido.Substring(0, 1) != mayusApellido)
                     {
-                        cantLetras1 = cantLetras1 + 1;
-                        nom1 += n;
+                        return "El apellido es incorrecto, debe empezar con mayusculas";
                     }
-                }
 
-                char a = '.';
-                foreach (var n in nombre[0])
-                {
-                    if (n != '.')
-                    {
-                    }
-                    else
-                    {
-                        a = n;
-                    }
-                }
-
-
-                foreach (var n in nombre[1])
-                {
-                    if (n != ' ')
-                    {
-                        cantLetras2 = cantLetras2 + 1;
-                        nom2 += n;
-                    }
-                }
-                char b = '.';
-                foreach (var n in nombre[1])
-                {
-                    if (n != '.')
-                    {
-                    }
-                    else
-                    {
-                        b = n;
-                    }
-                }
-                if (nombre[2] != 0 )
-                {
-
-                }
-                foreach (var n in nombre[2])
-                {
-                    if (n != ' ')
-                    {
-                        cantLetras3 = cantLetras3 + 1;
-                        nom3 += n;
-                    }
-                }
-
-                char c = '.';
-                foreach (var n in nombre[2])
-                {
-                    if (n != '.')
-                    {
-                    }
-                    else
-                    {
-                        c = n;
-                    }
-                }
-
-                //A--------------------------------------------------------------------------------------
-
-
-                if (cantLetras1 > 2 & a != '.')
-                {
-                    Console.WriteLine("El primer nombre: {0}, es correcto", nom1);
-                }
-
-                if (cantLetras1 == 2 & a == '.')
-                {
-                    Console.WriteLine("El primer nombre: {0}, es correcto", nom1);
-                }
-
-                //B--------------------------------------------------------------------------------------
-
-
-                if (cantLetras2 > 2 & b != '.')
-                {
-                    Console.WriteLine("El segundo nombre: {0}, es correcto", nom2);
-                }
-
-                if (cantLetras2 == 2 & b == '.')
-                {
-                    Console.WriteLine("El segundo nombre: {0}, es correcto", nom2);
-                }
-
-
-                //C--------------------------------------------------------------------------------------
-
-
-                if (cantLetras3 > 2 & c != '.')
-                {
-                    Console.WriteLine("El tercer nombre: {0}, es correcto", nom3);
-                }
-
-                if (cantLetras1 == 2 & c == '.')
-                {
-                    Console.WriteLine("El tercer nombre: {0}, es incorrecto", nom3);
                 }
 
             }
-                Console.ReadKey();
+            return "ok";
         }
 
+        public string ValidadorLargoYPunto(string PrimerNombre, string SegundoNombre, string Apellido)
+        {
+            // Recorremos el primer nombre buscando el '.' y evaluamos el largo del string para saber si es una palabra con punto o una letra con punto.
+            string mayusculaPrimerNombre = PrimerNombre.Substring(0, 1).ToUpper();
+            for (int i = 0; i < PrimerNombre.Length; i++)
+            {
+
+                string n = PrimerNombre.Substring(i);
+                if (n == ".")
+                {
+                    if (PrimerNombre.Length > 2)
+                    {
+                        return " En el primer nombre las abreviaciones solo pueden ser de una letra y un punto. EJ: 'S. Manuel Llanes'.";
+                    }
+                    else
+                    {
+                        if (PrimerNombre.Substring(0, 1) != mayusculaPrimerNombre)
+                        {
+                            return "El primer nombre es incorrecto.";
+                        }
+                    }
+                }
+
+            }
+
+            // Recorremos el segundo nombre buscando el '.' y evaluamos el largo del string para saber si es una palabra con punto o una letra con punto.
+            for (int i = 0; i < SegundoNombre.Length; i++)
+            {
+                string puntito = SegundoNombre.Substring(i);
+
+                if (puntito == ".")
+                {
+                    if (SegundoNombre.Length > 2)
+                    {
+                        return "En el segundo nombre las abreviaciones solo pueden ser de una letra y un punto. EJ: 'S. M. Llanes'.";
+                    }
+                }
+            }
+
+            // Recorremos el apellido buscando el '.' lo cual no esta permitido en los apellidos.
+            for (int i = 0; i < Apellido.Length; i++)
+            {
+                string puntito = Apellido.Substring(i);
+
+                if (puntito == ".")
+                {
+                    return "Los apellidos no pueden ser abreviados";
+                }
+            }
+            return "ok";
+        }
     }
-
-
-
-
 }
 
